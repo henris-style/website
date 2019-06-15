005994c51e954d33af4806bf001b5aedc2ab6cc1
Sil van Diepen <me@silvandiepen.com>
2019-06-15 18:04:25 +0200

    footer, examples and basic content wip

--- components/layout/footer.vue
+++ components/layout/footer.vue
@@ -1,8 +1,140 @@
 <template>
 	<footer id="footer" class="footer">
-		<h5 class="sub">
-			the end..
-		</h5>
+		<div class="row center">
+			<div class="column small-full medium-three-quarter footer__intro">
+				<div class="content">
+					<h2>
+						Henri's
+					</h2>
+				</div>
+			</div>
+			<div class="column small-full medium-three-quarter footer__navigation">
+				<div class="row">
+					<div class="column small-full medium-quarter footer__column">
+						<h4>About</h4>
+						<ul class="footer__nav-list">
+							<li class="footer__nav-item">
+								<nuxt-link class="footer__nav-link" to="/story">
+									The story
+								</nuxt-link>
+							</li>
+							<li class="footer__nav-item">
+								<nuxt-link class="footer__nav-link" to="/why">
+									Why?
+								</nuxt-link>
+							</li>
+							<li class="footer__nav-item">
+								<nuxt-link class="footer__nav-link" to="/help">
+									Help!
+								</nuxt-link>
+							</li>
+							<li class="footer__nav-item">
+								<nuxt-link class="footer__nav-link" to="/contributing">
+									Contributing
+								</nuxt-link>
+							</li>
+							<li class="footer__nav-item">
+								<nuxt-link class="footer__nav-link" to="/media">
+									Media
+								</nuxt-link>
+							</li>
+							<li class="footer__nav-item">
+								<nuxt-link class="footer__nav-link" to="/examples">
+									Examples
+								</nuxt-link>
+							</li>
+							<li class="footer__nav-item">
+								<nuxt-link class="footer__nav-link" to="/in-the-wild">
+									In the wild
+								</nuxt-link>
+							</li>
+						</ul>
+					</div>
+					<div class="column small-full medium-quarter footer__column">
+						<h4>Docs</h4>
+						<ul class="footer__nav-list">
+							<li class="footer__nav-item">
+								<a class="footer__nav-link" href="https://docs.henris.style/getting-started">
+									Getting Started
+								</a>
+							</li>
+							<li class="footer__nav-item">
+								<a class="footer__nav-link" href="https://docs.henris.style/installation">
+									Installation
+								</a>
+							</li>
+							<li class="footer__nav-item">
+								<a class="footer__nav-link" href="https://docs.henris.style/how-to-use">
+									How to use
+								</a>
+							</li>
+							<li class="footer__nav-item">
+								<a class="footer__nav-link" href="https://docs.henris.style/grid">
+									Grid
+								</a>
+							</li>
+							<li class="footer__nav-item">
+								<a class="footer__nav-link" href="https://docs.henris.style/colors">
+									Colors
+								</a>
+							</li>
+							<li class="footer__nav-item">
+								<a class="footer__nav-link" href="https://docs.henris.style/typography">
+									Typography
+								</a>
+							</li>
+							<li class="footer__nav-item">
+								<a class="footer__nav-link" href="https://docs.henris.style/mixins">
+									Mixins
+								</a>
+							</li>
+							<li class="footer__nav-item">
+								<a class="footer__nav-link" href="https://docs.henris.style/functions">
+									Functions
+								</a>
+							</li>
+						</ul>
+						<li class="footer__nav-item"></li>
+					</div>
+
+					<div class="column small-full medium-quarter footer__column">
+						<h4>More</h4>
+						<ul class="footer__nav-list">
+							<li class="footer__nav-item">
+								<a class="footer__nav-link" href="https://github.com/henris-style">
+									Github
+									<span>Henri's code is fully on Github.</span>
+								</a>
+							</li>
+							<li class="footer__nav-item">
+								<a class="footer__nav-link" href="https://codepen.io">
+									Codepen
+									<span>Some examples and the testing ground.</span>
+								</a>
+							</li>
+							<li class="footer__nav-item">
+								<a class="footer__nav-link" href="https://patreon.com">
+									Patreon
+									<span>Support Henri's</span>
+								</a>
+							</li>
+							<li class="footer__nav-item">
+								<nuxt-link class="footer__nav-link" to="/extensions">
+									Extensions
+									<span>Make it bigger</span>
+								</nuxt-link>
+							</li>
+							<li class="footer__nav-item">
+								<nuxt-link class="footer__nav-link" to="/tools">
+									Tools
+									<span>Some tools to kickstart your project.</span>
+								</nuxt-link>
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
 	</footer>
 </template>
 
@@ -11,11 +143,47 @@ export default {};
 </script>
 
 <style lang="scss">
-// @import '~tools';
+@import '~tools';
 
 // Footer styles
 .footer {
-	padding: grid(1);
-	text-align: center;
+	color: color(Blue);
+	padding: grid(2 0);
+	box-shadow: grid(1.5) 0 0 0 color(IceLight) inset;
+
+	@media #{$small-only} {
+		padding: $mobile-padding $mobile-padding $mobile-padding ($mobile-padding * 2);
+	}
+	&__column {
+		padding-top: grid(1);
+		h4 {
+			color: color(Blue);
+		}
+	}
+	&__intro {
+	}
+	&__navigation {
+	}
+	&__nav-list {
+	}
+	&__nav-item {
+		display: block;
+		padding: 0.5rem 0;
+	}
+	&__nav-link {
+		color: color(DarkDark);
+		text-decoration: none;
+		position: relative;
+		span {
+			opacity: 0.25;
+			position: absolute;
+			left: grid(3);
+			white-space: nowrap;
+			@media #{$small-only} {
+				left: grid(5);
+				top: 0;
+			}
+		}
+	}
 }
 </style>
--- components/section/examples.vue
+++ components/section/examples.vue
@@ -1,6 +1,51 @@
 <template>
-	<section class="intro">
-		Examples
+	<section class="examples">
+		<div class="row center">
+			<div class="column small-full medium-three-quarter">
+				<div class="content">
+					<h2>
+						A few examples
+					</h2>
+					<p>
+						Just a few, there is way more ofcourse in the documentation. But to give you an idea of what you can do and what
+						Henri’s will help you do.
+					</p>
+				</div>
+			</div>
+		</div>
+		<div class="scroller">
+			<div class="scroller__container">
+				<ul class="scroller__list">
+					<li class="scroller__item">
+						<span class="scroller__text">Grid</span>
+					</li>
+					<li class="scroller__item">
+						<span class="scroller__text">Menu</span>
+					</li>
+					<li class="scroller__item">
+						<span class="scroller__text">Colors</span>
+					</li>
+					<li class="scroller__item">
+						<span class="scroller__text">Animation</span>
+					</li>
+					<li class="scroller__item">
+						<span class="scroller__text">BEM</span>
+					</li>
+					<li class="scroller__item">
+						<span class="scroller__text">Gradients</span>
+					</li>
+					<li class="scroller__item">
+						<span class="scroller__text">Sizing</span>
+					</li>
+					<li class="scroller__item">
+						<span class="scroller__text">Typography</span>
+					</li>
+				</ul>
+			</div>
+		</div>
+		<div class="row center">
+			<div class="column small-full medium-three-quarter"></div>
+		</div>
 	</section>
 </template>
 
@@ -10,4 +55,34 @@ export default {};
 
 <style lang="scss">
 @import '~tools';
+.examples {
+	background-image: linear-gradient(to bottom, color(White), color(IceLight));
+	padding: grid(3 0);
+	.content {
+		// padding: grid(3) 0;
+		@media #{$small-only} {
+			padding: $mobile-padding;
+		}
+	}
+}
+.scroller {
+	&__container {
+		padding: grid(0.5 0);
+		overflow-x: scroll;
+	}
+	&__list {
+		display: flex;
+		flex-wrap: nowrap;
+	}
+	&__item {
+		white-space: nowrap;
+		font-size: grid(0.4);
+		@include min-(font-size, 0.4, 24);
+		& + & {
+			&::before {
+				content: '\00a0—\00a0';
+			}
+		}
+	}
+}
 </style>
