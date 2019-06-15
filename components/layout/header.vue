ef5e186a20b1b1a967247323fd1a488165d01a74
Sil van Diepen <me@silvandiepen.com>
2019-06-15 17:19:29 +0200

    add some filling for pages

--- components/layout/header.vue
+++ components/layout/header.vue
@@ -2,30 +2,36 @@
 	<header id="header" class="header">
 		<div class="row">
 			<div class="column small-half medium-fifth text-left">
-				henri's
-			</div>
-			<div class="column hide-for-small medium-three-fifth text-center">
-				<nuxt-link to="/story">
-					Story
-				</nuxt-link>
-				<nuxt-link to="/why">
-					Why
-				</nuxt-link>
-				<nuxt-link to="/help">
-					Help
-				</nuxt-link>
-				<nuxt-link to="/examples">
-					Examples
-				</nuxt-link>
-				<nuxt-link to="/in-the-wild">
-					In the wild
+				<nuxt-link to="/" class="header__logo">
+					henri's
 				</nuxt-link>
 			</div>
-			<div class="column small-half medium-fifth text-right">
-				<a href="https://docs.henris.style">Docs</a>
-				<nuxt-link to="/about">
-					About
-				</nuxt-link>
+			<div class="column hide-for-small medium-three-fifth text-center header__navigation">
+				<nav class="navigation">
+					<nuxt-link to="/story">
+						Story
+					</nuxt-link>
+					<nuxt-link to="/why">
+						Why
+					</nuxt-link>
+					<nuxt-link to="/help">
+						Help
+					</nuxt-link>
+					<nuxt-link to="/examples">
+						Examples
+					</nuxt-link>
+					<nuxt-link to="/in-the-wild">
+						In the wild
+					</nuxt-link>
+				</nav>
+			</div>
+			<div class="column small-half medium-fifth text-right header__subnavigation">
+				<nav class="navigation">
+					<a href="https://docs.henris.style">Docs</a>
+					<nuxt-link to="/about">
+						About
+					</nuxt-link>
+				</nav>
 			</div>
 		</div>
 	</header>
@@ -52,9 +58,14 @@ export default {
 	left: 0;
 
 	width: 100%;
-	padding: grid(2 3);
+	padding: grid(1.5 3);
 	@media #{$small-only} {
 		padding: $mobile-padding;
+	}
+	&__logo {
+		color: color(Blue);
+		font-weight: bold;
+		text-decoration: none;
 	}
 }
 .text-left {
@@ -65,5 +76,14 @@ export default {
 }
 .text-right {
 	text-align: right;
+}
+
+.navigation {
+	a {
+		text-decoration: none;
+		& + a {
+			margin-left: 1rem;
+		}
+	}
 }
 </style>
--- pages/help.vue
+++ pages/help.vue
@@ -1,6 +1,42 @@
 <template>
 	<div class="page page--help">
-		<h1>Help</h1>
+			<section class="basic-intro">
+			<div class="row center">
+				<div class="column small-full medium-three-quarter">
+					<div class="content">
+						<h1>Help</h1>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section class="basic-content">
+			<div class="row center">
+				<div class="column small-full medium-three-quarter">
+					<div class="content">
+						<p>
+							Nam ornare efficitur nibh. Morbi sem mauris, convallis non turpis ac, maximus lacinia urna. Nulla in molestie
+							leo. Proin laoreet rutrum nisl at porta. Praesent ullamcorper, ligula et suscipit ullamcorper, velit leo egestas
+							urna, dapibus volutpat felis est id felis. Nulla ac dui urna. Maecenas convallis pretium ligula a sagittis. Morbi
+							at tempor erat. Nunc vitae cursus tortor. In pulvinar id lectus id mattis. In faucibus ipsum sit amet enim cursus
+							euismod. Nullam ut scelerisque nibh. Integer dignissim velit ac gravida suscipit. Donec lorem libero, gravida a
+							molestie id, finibus et ligula. Aenean posuere, arcu nec dignissim sagittis, nisi ex vestibulum erat, sit amet
+							feugiat urna felis id purus.
+						</p>
+						<p>
+							Quisque imperdiet, est et porttitor molestie, sem odio imperdiet enim, a euismod elit nisi id ex. Donec consequat
+							cursus odio in porta. Fusce condimentum tellus non nunc aliquam lacinia. Aenean semper felis pulvinar lectus
+							molestie, in maximus sapien egestas. Phasellus eget pellentesque metus. Integer eleifend est in arcu posuere, sed
+							tempus turpis aliquam. Nullam arcu sapien, aliquet sed sem ac, auctor pulvinar urna. In fermentum tellus in leo
+							tempus aliquet. Duis consectetur, mauris ut ornare luctus, nisi nisi sollicitudin erat, in pulvinar augue ipsum
+							accumsan erat. Maecenas commodo libero sem, sed faucibus nisi rutrum et. Suspendisse consequat id magna eget
+							malesuada. Vestibulum gravida neque in volutpat luctus. Mauris dolor dui, scelerisque vitae velit non, cursus
+							cursus augue. Fusce vel arcu nec ipsum condimentum egestas.
+						</p>
+					</div>
+				</div>
+			</div>
+		</section>
 	</div>
 </template>
 
