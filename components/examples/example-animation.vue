d43799c887698d8537720c2c925f63968deb954d
Sil van Diepen <me@silvandiepen.com>
2019-06-15 21:26:15 +0200

    examples wip

---
+++ components/examples/example-animation.vue
@@ -0,0 +1,11 @@
+<template>
+	<div class="example-menu">
+		<h4>Exampleee Animation</h4>
+	</div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
--- components/section/examples.vue
+++ components/section/examples.vue
@@ -13,44 +13,90 @@
 				</div>
 			</div>
 		</div>
+
 		<div class="scroller">
 			<div class="scroller__container">
 				<ul class="scroller__list">
-					<li class="scroller__item">
-						<span class="scroller__text">Grid</span>
+					<li v-for="(example, idx) in examples" :key="idx" class="scroller__item">
+						<button
+							class="scroller__link"
+							:class="[example.name.toLowerCase() == active ? 'scroller__link--active' : '']"
+							@click="active = example.name.toLowerCase()"
+						>
+							<span class="scroller__text">{{ example.name }}</span>
+						</button>
 					</li>
-					<li class="scroller__item">
-						<span class="scroller__text">Menu</span>
+					<li v-for="(example, idx) in examples" :key="idx" class="scroller__item">
+						<button
+							class="scroller__link"
+							:class="[example.name.toLowerCase() == active ? 'scroller__link--active' : '']"
+							@click="active = example.name.toLowerCase()"
+						>
+							<span class="scroller__text">{{ example.name }}</span>
+						</button>
 					</li>
-					<li class="scroller__item">
-						<span class="scroller__text">Colors</span>
-					</li>
-					<li class="scroller__item">
-						<span class="scroller__text">Animation</span>
-					</li>
-					<li class="scroller__item">
-						<span class="scroller__text">BEM</span>
-					</li>
-					<li class="scroller__item">
-						<span class="scroller__text">Gradients</span>
-					</li>
-					<li class="scroller__item">
-						<span class="scroller__text">Sizing</span>
-					</li>
-					<li class="scroller__item">
-						<span class="scroller__text">Typography</span>
+					<li v-for="(example, idx) in examples" :key="idx" class="scroller__item">
+						<button
+							class="scroller__link"
+							:class="[example.name.toLowerCase() == active ? 'scroller__link--active' : '']"
+							@click="active = example.name.toLowerCase()"
+						>
+							<span class="scroller__text">{{ example.name }}</span>
+						</button>
 					</li>
 				</ul>
 			</div>
 		</div>
 		<div class="row center">
-			<div class="column small-full medium-three-quarter"></div>
+			<div class="column small-full medium-three-quarter">
+				<div
+					v-for="(example, idx) in examples"
+					:key="idx"
+					class="example"
+					:class="[example.name.toLowerCase() == active ? 'example--active' : '']"
+				>
+					<ExampleGrid v-if="example.name.toLowerCase() === 'grid'" />
+					<ExampleMenu v-if="example.name.toLowerCase() === 'menu'" />
+					<ExampleColors v-if="example.name.toLowerCase() === 'colors'" />
+					<ExampleAnimation v-if="example.name.toLowerCase() === 'animation'" />
+					<ExampleBEM v-if="example.name.toLowerCase() === 'bem'" />
+					<ExampleGradients v-if="example.name.toLowerCase() === 'gradients'" />
+					<ExampleSizing v-if="example.name.toLowerCase() === 'sizing'" />
+					<ExampleTypography v-if="example.name.toLowerCase() === 'typography'" />
+				</div>
+			</div>
 		</div>
 	</section>
 </template>
 
 <script>
-export default {};
+export default {
+	components: {
+		ExampleGrid: () => import('~/components/examples/example-grid.vue'),
+		ExampleMenu: () => import('~/components/examples/example-menu.vue'),
+		ExampleColors: () => import('~/components/examples/example-colors.vue'),
+		ExampleAnimation: () => import('~/components/examples/example-animation.vue'),
+		ExampleBEM: () => import('~/components/examples/example-bem.vue'),
+		ExampleGradients: () => import('~/components/examples/example-gradients.vue'),
+		ExampleSizing: () => import('~/components/examples/example-sizing.vue'),
+		ExampleTypography: () => import('~/components/examples/example-typography.vue')
+	},
+	data() {
+		return {
+			active: '',
+			examples: [
+				{ name: 'Grid', active: false },
+				{ name: 'Menu', active: false },
+				{ name: 'Colors', active: false },
+				{ name: 'Animation', active: false },
+				{ name: 'BEM', active: false },
+				{ name: 'Gradients', active: false },
+				{ name: 'Sizing', active: false },
+				{ name: 'Typography', active: false }
+			]
+		};
+	}
+};
 </script>
 
 <style lang="scss">
@@ -59,23 +105,43 @@ export default {};
 	background-image: linear-gradient(to bottom, color(White), color(IceLight));
 	padding: grid(3 0);
 	.content {
-		// padding: grid(3) 0;
 		@media #{$small-only} {
 			padding: $mobile-padding;
 		}
 	}
 }
 .scroller {
+	// position: absolute;
+	z-index: 2;
 	&__container {
+		position: relative;
 		padding: grid(0.5 0);
-		overflow-x: scroll;
+		overflow: hidden;
+		width: 100vw;
 	}
 	&__list {
+		position: relative;
+		left: 50%;
+		transform: translateX(-50%);
 		display: flex;
 		flex-wrap: nowrap;
 	}
+	&__link {
+		text-decoration: none;
+		border: none;
+		background: none;
+		&:focus {
+			outline: none;
+		}
+		&--active {
+			.scroller__text {
+				color: color(Blue);
+			}
+		}
+	}
 	&__item {
 		white-space: nowrap;
+		color: color(Ice);
 		font-size: grid(0.4);
 		@include min-(font-size, 0.4, 24);
 		& + & {
@@ -83,6 +149,24 @@ export default {};
 				content: '\00a0—\00a0';
 			}
 		}
+	}
+	&__text {
+		font-size: grid(0.4);
+		@include min-(font-size, 0.4, 24);
+		color: color(Ice);
+	}
+}
+
+.example {
+	padding: grid(1 0);
+	position: fixed;
+	opacity: 0;
+	clip-path: inset(0 100% 0 0);
+	&--active {
+		position: relative;
+		opacity: 1;
+		clip-path: inset(0 0% 0 0);
+		transition: opacity 0.5s, clip-path 2s;
 	}
 }
 </style>
